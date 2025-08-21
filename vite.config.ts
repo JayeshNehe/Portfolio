14312ca4799f4fb151948e4b06904c21eb8ab6c1
Jayesh Nehe <jayeshbnehe@gmail.com>
2025-08-21 19:26:34 +0530

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/Portfolio/",   // 👈 add this line (must match your repo name exactly, case-sensitive)
   plugins: [react()],
   resolve: {
     alias: {
